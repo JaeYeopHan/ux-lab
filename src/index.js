cb96c78e6eadc5181d7aa011302ce3e53a2d30c6
JaeYeopHan <ljyhanll@gmail.com>
2019-08-08 14:11:11 +0900

    feat: Add modal component

--- src/index.js
+++ src/index.js
@@ -5,28 +5,49 @@ import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
 import './reset.css'
 import './index.css'
 
-function InputPage() {
+function WorkSpace() {
   const [isFocus, setFocus] = React.useState(false)
+  const [isModalOpen, toggleModal] = React.useState(false)
 
+  const closeModal = (e) => {
+    if (e.target.classList.contains('layer_wrap')) {
+      toggleModal(false)
+    }
+  }
+
+  console.log('isModalOpen: ', isModalOpen)
+  
   return (
-    <div className={`container ${isFocus ? 'input-focused' : ''}`}>
-      <input
-        className="input"
-        type="text"
-        name="name"
-        onFocus={(e) => {
-          setFocus(true)
-        }}
-        onBlur={(e) => {
-          setFocus(false)
-        }}
-        autoComplete="off"
-        autoFocus={true}
-      />
-      <div>
-        <button className="button"></button>
+    <>
+      <div className={`container ${isFocus ? 'input-focused' : ''}`}>
+        <input
+          className="input"
+          type="text"
+          name="name"
+          onFocus={(e) => {
+            setFocus(true)
+          }}
+          onBlur={(e) => {
+            setFocus(false)
+          }}
+          autoComplete="off"
+          autoFocus={true}
+        />
+        <button className="modal-trigger-button" onClick={() => toggleModal(true)}>modal open</button>
+        <div>
+          <button className="button">NEXT</button>
+        </div>
       </div>
-    </div>
+      {isModalOpen && (
+      <div className="layer_wrap _dialog" onClick={closeModal}>
+        <div className="layer_inner layer_v1">
+          <p className="desc portals_scroll">
+            <input className="input" autoFocus={true}></input>
+          </p>
+        </div>
+      </div>
+    )}
+    </>
   );
 }
 
@@ -43,7 +64,7 @@ function App() {
   return (
     <Router>
       <Switch>
-        <Route path='/input' component={InputPage} />
+        <Route path='/input' component={WorkSpace} />
         <Route exact={true} path='/' component={HomePage} />
       </Switch>
     </Router>
