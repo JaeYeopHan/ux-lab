486d106681cfc982c9fce917dd964223c35360e6
JaeYeopHan <ljyhanll@gmail.com>
2019-08-24 16:37:59 +0900

    fix: Extract input css from index.css

--- src/components/input/InputWorkSpace.jsx
+++ src/components/input/InputWorkSpace.jsx
@@ -1,17 +1,19 @@
-import React from 'react';
+import React from 'react'
+
+import './Input.css'
 
 export function InputWorkSpace() {
   const [isModalOpen, toggleModal] = React.useState(false)
 
   const controlledInput = React.useRef()
 
-  const onClickContainer = (e) => {
+  const onClickContainer = e => {
     if (e.target.classList.contains('close_modal')) {
       toggleModal(false)
     }
   }
 
-  const onClickLayer = (e) => {
+  const onClickLayer = e => {
     if (e.target.classList.contains('focus_input')) {
       controlledInput && controlledInput.current.focus()
     }
@@ -19,29 +21,25 @@ export function InputWorkSpace() {
 
   return (
     <>
-      <div className='container'>
-        <input
-          className="input"
-          type="text"
-          name="name"
-          autoComplete="off"
-          autoFocus={true}
-        />
-        <button className="modal-trigger-button" onClick={() => toggleModal(true)}>modal open</button>
+      <div className="container">
+        <input className="input" type="text" name="name" autoComplete="off" autoFocus={true} />
+        <button className="modal-trigger-button" onClick={() => toggleModal(true)}>
+          modal open
+        </button>
         <div>
           <button className="button">NEXT</button>
         </div>
       </div>
       {isModalOpen && (
-      <div className="layer_wrap close_modal" onClick={onClickContainer}>
-        <div className="layer_inner layer_v1 focus_input" onClick={onClickLayer}>
-          <p className="desc">
-            <input className="input" type="tel" autoFocus={true} ref={controlledInput}></input>
-            <button onClick={() => toggleModal(false)}>close</button>
-          </p>
+        <div className="layer_wrap close_modal" onClick={onClickContainer}>
+          <div className="layer_inner layer_v1 focus_input" onClick={onClickLayer}>
+            <p className="desc">
+              <input className="input" type="tel" autoFocus={true} ref={controlledInput}></input>
+              <button onClick={() => toggleModal(false)}>close</button>
+            </p>
+          </div>
         </div>
-      </div>
-    )}
+      )}
     </>
-  );
+  )
 }
